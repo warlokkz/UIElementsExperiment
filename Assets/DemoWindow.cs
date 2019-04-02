7a40c5529d2754aa7ce7e58587ece82fd1dca6e9
warlokkz <alexander.anich@gmail.com>
2019-04-01 23:51:39 -0700

    experimenting with layout using flexbox api

--- Assets/DemoWindow.cs
+++ Assets/DemoWindow.cs
@@ -4,6 +4,17 @@ using UnityEngine.UIElements;
 
 public class DemoWindow : EditorWindow
 {
+    private readonly Color[] _colors =
+    {
+        Color.blue,
+        Color.green,
+        Color.yellow
+    };
+
+    private readonly int kMargin = 50;
+    private readonly int kPadding = 10;
+    private readonly int kBoxSize = 100;
+    
     private void OnEnable()
     {
         var root = rootVisualElement;
@@ -18,10 +29,10 @@ public class DemoWindow : EditorWindow
                 backgroundColor = new Color(0.3f, 0.3f, 0.3f),
             }
         };
-
+        
         container.Add(new Label
         {
-            text = "Hello, World!",
+            text = "UI Elements",
             style =
             {
                 fontSize = 20,
@@ -31,6 +42,70 @@ public class DemoWindow : EditorWindow
         });
         
         root.Add(container);
+
+        var grid = new VisualElement
+        {
+            style =
+            {
+                display = DisplayStyle.Flex,
+                flexDirection = FlexDirection.Row,
+                justifyContent = Justify.SpaceBetween
+            }
+        };
+        
+        for (var i = 0; i < _colors.Length; i++)
+        {
+            grid.Add(
+                BoxColumn(i)
+            );
+        }
+        
+        root.Add(grid);
+    }
+
+    private VisualElement BoxColumn(int index)
+    {
+        bool flipped = index % 2 == 0;
+        var direction = flipped ?
+             FlexDirection.Column : FlexDirection.ColumnReverse;
+        
+        var row =  new VisualElement
+        {
+            style =
+            {
+                display = DisplayStyle.Flex,
+                flexDirection = direction,
+                left = kMargin,
+                top = flipped ? 0 : kMargin,
+                bottom = flipped ? kMargin : 0,
+                height = kBoxSize * _colors.Length,
+                width = kBoxSize * _colors.Length
+            }
+        };
+        
+        for (var i = 0; i < _colors.Length; i++)
+        {
+            var color = _colors[i];
+            
+            row.Add(
+                BoxElement(color)
+            );
+        }
+
+        return row;
+    }
+
+    private VisualElement BoxElement(Color color)
+    {
+        return new VisualElement
+        {
+            style =
+            {
+                width = kBoxSize,
+                height = kBoxSize,
+                backgroundColor = color
+            }
+        };
     }
     
     #region Show Window
